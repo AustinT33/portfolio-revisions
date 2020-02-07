3fdc6ae64526181ae649d3ee3b07102a6bdf15de
AustinT33 <austintumlinson@gmail.com>
2020-02-07 14:41:19 -0600

    testing for multiple screens

---
+++ index.js
@@ -0,0 +1,14 @@
+$('#about').click(function(e) {
+    e.preventDefault();
+    $('.about').toggle();
+})
+
+$('#projects').click(function(e) {
+    e.preventDefault();
+    $('section').toggle();
+})
+
+$('#contact').click(function(e) {
+    e.preventDefault();
+    $('.contact').toggle();
+})
